091acd4992a0b1fccd1a14e6d50ba9fac884dd42
i1993sk-code <i1993sk@gmail.com>
2026-06-03 11:34:28 +0530

    fix: manual CORS headers for Express 5

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 
 app.get(['/', '/api/health'], (req, res) => {
