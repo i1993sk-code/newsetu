a1b61a60811efd1f4bacc610bd762762dae0b627
i1993sk-code <i1993sk@gmail.com>
2026-06-03 10:30:25 +0530

    fix: clean duplicate code in server/index.js

--- server/index.js
+++ server/index.js
@@ -5,16 +5,11 @@ const cors = require('cors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Request:', req.method, req.path);
-  next();
-});
-
 app.use(cors());
 app.use(express.json());
 
 app.get(['/', '/api/health'], (req, res) => {
-  res.status(200).json({ status: 'ok', message: 'NewSetu API is running' });
+  res.json({ status: 'ok', message: 'NewSetu API is running' });
 });
 
 const providerRoutes = require('./routes/provider');
@@ -35,21 +30,3 @@ async function start() {
 }
 
 start();
-
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB error:', err));
-
-const providerRoutes = require('./routes/provider');
-app.use('/api/provider', providerRoutes);
-
-
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
