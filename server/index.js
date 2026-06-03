37f1d284a96e72492053fefd836a18ee6b421be5
i1993sk-code <i1993sk@gmail.com>
2026-06-03 10:58:21 +0530

    fix: revert to simple listen pattern, remove 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -17,16 +17,10 @@ app.use('/api/provider', providerRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-async function start() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.log('MongoDB error:', err.message);
-  }
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB error:', err));
 
-start();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
